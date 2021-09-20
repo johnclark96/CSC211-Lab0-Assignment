d8b7297ddb89cc2ea03bd9c7a2affdef98cac3fe
johnclark96 <mlitvinkox96@gmail.com>
2021-09-20 13:02:58 -0400

    Add project files.

---
+++ CSC211 Lab0 Assignment/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int month;
+	int date;
+	int year;
+
+	cout << "Please Enter Todays Date" << endl;
+	cin >> month >> date >> year;
+	cout << month << "/" << date << "/" << year << endl;
+
+	system("PAUSE");
+	return 0;
+}
