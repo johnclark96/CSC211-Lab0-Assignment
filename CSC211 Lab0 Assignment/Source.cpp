84a6306fbdc7e6fee1dadf1cfdbd50ae7aaa4448
johnclark96 <mlitvinkox96@gmail.com>
2021-09-20 13:21:52 -0400

    Added basic leap year checker

--- CSC211 Lab0 Assignment/Source.cpp
+++ CSC211 Lab0 Assignment/Source.cpp
@@ -4,13 +4,30 @@ using namespace std;
 
 int main() {
 
-	int month;
-	int date;
-	int year;
+	int month = 0;
+	int date = 0;
+	int year = 0;
 
 	cout << "Please Enter Todays Date" << endl;
 	cin >> month >> date >> year;
 	cout << month << "/" << date << "/" << year << endl;
+
+	if (year % 4 == 0) {
+		if (year % 100 == 0) {
+			if (year % 400 == 0) {
+				cout << year << " is leap year" << endl;
+			}
+			else {
+				cout << year << " is not leap year" << endl;
+			}
+		}
+		else {
+			cout << year << " is leap year" << endl;
+		}
+	}
+	else {
+		cout << year << " is not leap year" << endl;
+	}
 
 	system("PAUSE");
 	return 0;
